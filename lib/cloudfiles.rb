9abb648feec7c2395a237b26305471c5565b195d
Dewey <dewey@dewey-desktop.(none)>
2011-12-05 19:53:21 -0600

    Fixes issue with spaces causing 404s.

--- lib/cloudfiles.rb
+++ lib/cloudfiles.rb
@@ -49,9 +49,9 @@ module CloudFiles
     (str.respond_to?(:lines) ? str.lines : str).to_a.map { |x| x.chomp }
   end
 
-  # CGI.escape, but without special treatment on spaces
+  # Escapes characters so the API doesn't return 404s. 
   def self.escape(str,extra_exclude_chars = '')
-    str.gsub(/([^a-zA-Z0-9\_\.\-#{extra_exclude_chars}]+)/) do
+    str.gsub(/([^a-zA-Z0-9\s\_\.\-#{extra_exclude_chars}]+)/) do
       '%' + $1.unpack('H2' * $1.bytesize).join('%').upcase
     end
   end
